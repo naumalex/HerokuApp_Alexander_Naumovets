24280b68d7955fa21c2493c88c8536ae78964694
alexn <naum1979alex@gmail.com>
2022-07-01 00:19:31 +0300

    Task 1. Add delete elements

---
+++ src/test/java/AddRemoveElementsTest.java
@@ -0,0 +1,43 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class AddRemoveElementsTest extends BaseTest {
+
+    @BeforeMethod
+    public void navigate() { driver.get("https://the-internet.herokuapp.com/add_remove_elements/"); }
+
+    public void addElement() {
+        WebElement addElementButton = driver.findElement(
+                By.xpath("//button[text()='Add Element']"));
+        addElementButton.click();
+    }
+
+    public void deleteElement() {
+        WebElement deleteButton = driver.findElement(
+                By.xpath("//button[text()='Delete']"));
+        deleteButton.click();
+    }
+    public int getElementsCount() {
+        List<WebElement> deleteButtons = driver.findElements(
+                By.xpath("//button[text()='Delete']"));
+        return deleteButtons.size();
+    }
+
+    @Test
+    public void addRemoveElementsTest() {
+        final int EXPECTED_NUMBER_OF_ELEMENTS = 1;
+        addElement();
+        addElement();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        deleteElement();
+        Assert.assertEquals(getElementsCount(), EXPECTED_NUMBER_OF_ELEMENTS,
+                "Number of added elements is different from the expected value");
+    }
+}
+
