00aab8403e2b7099a5314c6adacb2f4535dff180
alexn <naum1979alex@gmail.com>
2022-07-17 20:59:26 +0300

    Added homework 10

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,22 +1,32 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
-    protected WebDriver driver;
 
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected void waitUntilMessage(By locator) {
+        wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
     @BeforeClass
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 30);
     }
 
     @AfterClass
-    public void quitDriver() { driver.quit(); }
+    public void quitDriver() {
+        driver.quit();
+    }
 }
